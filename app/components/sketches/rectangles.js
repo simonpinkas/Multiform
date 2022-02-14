b7aef9f47c79d64643171c812047823e3d403bf4
E1nst1n <jonas.rapin@hotmail.com>
2022-02-14 13:54:41 +0100

    Test pattern

--- app/components/sketches/rectangles.js
+++ app/components/sketches/rectangles.js
@@ -1,18 +1,37 @@
 import { sketches, sketchWidth, sketchHeight } from "../sketches";
 
-const rectangles = {
-  name: "Rectangles",
+const swirling = {
+  name: "Swirling",
   sketch: (sketch) => {
     sketch.setup = () => {
       let cnv = sketch.createCanvas(sketchWidth, sketchHeight);
       sketch.pixelDensity(2);
       cnv.id("drawing-canvas");
       cnv.class("control-target");
-      sketch.background(0, 255, 255, 255);
+      sketch.background(0);
+      noiseDetail(1, 0);
+      genNoiseImg();
+      image(noiseImg, 0, 0);
     };
     sketch.draw = () => {
-      sketch.rect(sketch.mouseX, sketch.mouseY, 100, 100);
+      noiseImg = createGraphics(width, height);
+      noisImg.loadPixels();
+      var widthd = width*pixelDensity();
+      var heightd = height*pixelDensity();
+      for(var i=0; i<widthd; i++){
+        for (var j=0; j<heightd; j++){
+          var x = i/pixelDensity();
+          var y = j/pixelDensity();
+          var bright = pow(noise(x/noiseScale, y/noiseScale)-0.3, 1/2.0)*400;
+          noiseImg.pixels[(i+j*widthd)*4] = bright;
+          noiseImg.pixels[(i+j*widthd)*4+1] = bright;
+          noiseImg.pixels[(i+j*widthd)*4+2] = bright;
+          noiseImg.pixels[(i+j*widthd)*4+3] = 255;
+        }
+      }
+      noiseImg.updatePixels();
     };
   }
 };
-sketches.push(rectangles);
+sketches.push(swirling);
+
